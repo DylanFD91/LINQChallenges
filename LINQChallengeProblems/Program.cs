62bdce040b4df5acdf3117bf6712e79901061be2
Dylan DeRouchie <dylan.f.derouchie@gmail.com>
2020-03-09 11:28:14 -0500

    set up

--- LINQChallengeProblems/Program.cs
+++ LINQChallengeProblems/Program.cs
@@ -10,6 +10,42 @@ namespace LINQChallengeProblems
     {
         static void Main(string[] args)
         {
+            /*1. Using LINQ, write a function that returns all words that contain the substring “th” from a list.
+                List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", “mathematics” };
+            */
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+
+
+
+            /*
+            2. Using LINQ, write a function that takes in a list of strings and returns a copy of the list without duplicates.
+                List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            */
+
+
+
+
+            /*
+            3. Using LINQ, write a function that calculates the class grade average after dropping the lowest grade for each student. The function should take in a list of strings of grades (e.g., one string might be "90,100,82,89,55"), drops the lowest grade from each string, averages the rest of the grades from that string, then averages the averages.
+                List<string> classGrades = new List<string>()
+                {
+                "80,100,92,89,65",
+                "93,81,78,84,69",
+                "73,88,83,99,64",
+                "98,100,66,74,55"
+                 };
+                Expected output: 86.125
+            */
+
+
+
+
+
+
+            /*
+                4. Write a function that takes in a string of letters (i.e. “Terrill”) and returns an alphabetically ordered string corresponding to the letter frequency (i.e. "E1I1L2R2T1")
+
+             */
         }
     }
 }
