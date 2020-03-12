069f97eaa269998f7bddaae739ceb941f4ec21e3
Danielle Demke <danielledemke@gmail.com>
2020-03-12 07:56:00 -0500

    Started working on problem 4. Will resubmit when problems are solved

--- LINQChallengeProblems/Program.cs
+++ LINQChallengeProblems/Program.cs
@@ -35,27 +35,47 @@ namespace LINQChallengeProblems
             Console.ReadLine();
 
 
-            /*
-            3. Using LINQ, write a function that calculates the class grade average after dropping the lowest grade for each student. The function should take in a list of strings of grades (e.g., one string might be "90,100,82,89,55"), drops the lowest grade from each string, averages the rest of the grades from that string, then averages the averages.
-                List<string> classGrades = new List<string>()
-                {
+
+            //    3.Using LINQ, write a function that calculates the class grade average after dropping the lowest grade for each student.The function should take in a list of strings of grades (e.g., one string might be "90,100,82,89,55"), drops the lowest grade from each string, averages the rest of the grades from that string, then averages the averages.
+            //      List<string> classGrades = new List<string>()
+            //      {
+            //        "80,100,92,89,65",
+            //        "93,81,78,84,69",
+            //        "73,88,83,99,64",
+            //        "98,100,66,74,55"
+            //       };
+            //Expected output: 86.125
+
+            List<string> classGrades = new List<string>()
+              {
                 "80,100,92,89,65",
                 "93,81,78,84,69",
                 "73,88,83,99,64",
                 "98,100,66,74,55"
-                 };
-                Expected output: 86.125
-            */
+               };
+            
+            classGrades.ForEach(m => m.Split(',').ToArray().Select(s => int.Parse(s)).OrderByDescending(h => h).Select(n => n != m.Min()).ToList().Average(x => m.Length));
 
 
 
 
 
 
-            /*
-                4. Write a function that takes in a string of letters (i.e. “Terrill”) and returns an alphabetically ordered string corresponding to the letter frequency (i.e. "E1I1L2R2T1")
 
-             */
+
+            //  4. Write a function that takes in a string of letters (i.e. “Terrill”) and returns an alphabetically ordered string corresponding to the letter frequency (i.e. "E1I1L2R2T1")
+
+
+            string example = "Danielle";
+            example.ToCharArray();
+            char[] answer = example.Distinct().Count();
+            foreach(char x in example)
+            {
+                Console.WriteLine(x);
+            }
+
+            
+             
         }
     }
 }
